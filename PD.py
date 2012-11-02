5d9c7ea4c8253fde42ea327ee14fc11356335fde
Nate nate <heidtn@umich.edu>
2012-11-02 16:24:48 -0400

    added PD controller.  Takes in basic gains and adjusts accordingly.

--- PD.py
+++ PD.py
@@ -1,4 +1,21 @@
-class PD( self ):
+class PD:
   def __init__( self, pgain, dgain):
-    self.dummy = 1
-  def add_value( self ) :
+    self.pgain = pgain
+    self.dgain = dgain
+    self.first_call = True
+
+  def process( self, error, timestamp):
+    if self.first_call: #if this is the first function call
+      self.prev_error = error  #initialize the prev_value to value
+      self.first_call = False
+    self.P = error
+    self.D = (error - self.prev_error)/timestamp
+    self.prev_error = error
+    self.val = self.pgain*self.P + self.dgain*self.D
+    return self.val
+
+  def set_pgain(self, pgain):
+    self.pgain = pgain
+
+  def set_dgain(self, dgain):
+    self.dgain = dgain 
